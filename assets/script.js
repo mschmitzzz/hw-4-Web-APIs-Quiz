5d3d7720a6bb8a42038a2a3a90a1332d93d2c463
Mac Schmitz <mac.schmitz@att.net>
2022-04-07 23:05:50 -0400

    Added all the questions in the questions array

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ var questionEl = document.querySelector("#question");
 var answersEl = document.querySelector("#possibleAnswers");
 
 var hideClass = "hide";
+
 var questions = [
     {
         question: "George Clinton is the front-man for which of funk music's most influential bands?",
@@ -18,8 +19,48 @@ var questions = [
     },
     {
         question: "Bootsy Collins was the bass player for which iconic funk band?",
-        answers: ["James Brown", "Parliament-Funkadelic", "Both"],
+        answers: ["James Brown", "Parliament-Funkadelic", "Sly & The Family Stone", "James Brown and Parliament Funkadelic"],
         answer: 0
+    }
+    {
+        question: "Who is credited with creating the funk beat in the late 1950s and early 1960s?",
+        answers: ["George Clinton", "James Brown", "Elvis Presley", "The Beatles"],
+        answer: 1
+    }
+    {
+        question: "Which band recorded the first slap bass lick?",
+        answers: ["Grand Funk Railroad", "The Gap Band", "War", "Sly & The Family Stone"],
+        answer: 3
+    }
+    {
+        question: 'Who emerged as the "Queen of Funk" in the 1970s and 80s with her band Rufus?',
+        answers: ["Aretha Franklin", "Lynn Collins", "Chaka Khan", "Etta James"],
+        answer: 2
+    }
+    {
+        question: 'Which New Orleans base funk band release their hit "Cissy Strut" in 1969?',
+        answers: ["The Neville Brothers", "The Meters", "Allen Toussaint", "John Cleary"],
+        answer: 1
+    }
+    {
+        question: "Which of the following characteristics is not commonly found in funk music?",
+        answers: ["Syncopation", "Strong downbeats", "Seventh chord variants", "Grooves driven by violin"],
+        answer: 3
+    }
+    {
+        question: "Bootsy Collins, Larry Graham, George Porter, Jr., and Marcus Miller are all considered among the best funk players at which instrument?",
+        answers: ["Guitar", "Bass", "Drums", "Trombone"],
+        answer: 1
+    }
+    {
+        question: "Jabo Starks, Clyde Stubblefield, Ziggaboo Modeliste, and David Garibaldi are all considered pioneers of funk on which instrument?",
+        answers: ["Guitar", "Bass", "Drums", "Trombone"],
+        answer: 2
+    }
+    {
+        question: "Although funk music is still around and popular in its own right, which other modern genre of music did it most inspire?",
+        answers: ["Country", "EDM", "Pop", "Hip hop"],
+        answer: 3
     }
 ];
 var currentQuestion = 0;
