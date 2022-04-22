b8c32431087fd7feb664259c39052d95c7ffe357
Mac Schmitz <mac.schmitz@att.net>
2022-04-21 22:44:06 -0400

    reworked questions arry with answers objects, new CSS, new answer button format

--- assets/script.js
+++ assets/script.js
@@ -1,177 +1,174 @@
-// identify HTML elements that will be needed
-var screen0El = document.querySelector("#screen0");
-var screen0ButtonEl = screen0El.querySelector("button");
-var screen1El = document.querySelector("#screen1");
-var screen1ButtonEl = screen1El.querySelector("button");
-var screen2El = document.querySelector("#screen2");
-var screen2ButtonEl = screen2El.querySelector("button");
-var highScoreEl = document.querySelector("#highScore");
-var timerEl = document.querySelector("#timer");
-var questionEl = document.querySelector("#question");
-var answersEl = document.querySelector("#possibleAnswers");
+var startButtonEl = document.getElementById("start-btn");
+var nextButtonEl = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
 
-// creates ability to toggle screens
-var hideClass = "hide";
+var shuffledQuestions, currentQuestionIndex
+
+startButtonEl.addEventListener("click", startGame);
+nextButtonEl.addEventListener("click", function() {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+//loads the question container
+function startGame() {
+    console.log("Started")
+    startButtonEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove("hide");
+    setNextQuestion();
+};
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(function(answer) {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        };
+        button.addEventListener("click", selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButtonEl.classList.add("hide");
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+function selectAnswer() {
+    var selectedButton = event.target;
+    var correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if(shuffledQuestions.length > currentQuestionIndex +1) {
+        nextButtonEl.classList.remove("hide");
+    } else {
+        startButtonEl.innerText = "Restart";
+        startButtonEl.classList.remove("hide");
+    }
+    
+};
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+};
 
 //array of all 10 quiz questions
 var questions = [
     {
-        question: "George Clinton is the front-man for which of funk music's most influential bands?",
-        answers: ["Parliament", "Funkadelic", "Parliament-Funkadelic", "All of the above"],
-        answer: 3
+        question: "George Clinton is the front-man for which of funk music's most influential band(s)?",
+        answers: [
+            {text: "Parliament", correct: false},
+            {text: "Funkadelic", correct: false},
+            {text: "Parliament-Funkadelic", correct: false},
+            {text: "All of the above", correct: true}
+        ]
     },
     {
         question: "Bootsy Collins was the bass player for which iconic funk band?",
-        answers: ["James Brown", "Parliament-Funkadelic", "Sly & The Family Stone", "James Brown and Parliament Funkadelic"],
-        answer: 0
+        answers: [
+            {text: "James Brown", correct: true},
+            {text: "Parliament-Funkadelic", correct: false},
+            {text: "Sly & The Family Stone", correct: false},
+            {text: "James Brown and Parliament Funkadelic", correct: false}
+        ]
     },
     {
         question: "Who is credited with creating the funk beat in the late 1950s and early 1960s?",
-        answers: ["George Clinton", "James Brown", "Elvis Presley", "The Beatles"],
-        answer: 1
+        answers: [
+            {text: "George Clinton", correct: false},
+            {text: "James Brown", correct: true},
+            {text: "Elvis Presley", correct: false},
+            {text: "The Beatles", correct: false}
+        ]
     },
     {
         question: "Which band recorded the first slap bass lick?",
-        answers: ["Grand Funk Railroad", "The Gap Band", "War", "Sly & The Family Stone"],
-        answer: 3
+        answers: [
+            {text: "Grand Funk Railroad", correct: false},
+            {text: "The Gap Band", correct: false},
+            {text: "War", correct: false},
+            {text: "Sly & The Family Stone", correct: true}
+        ]
     },
     {
         question: 'Who emerged as the "Queen of Funk" in the 1970s and 80s with her band Rufus?',
-        answers: ["Aretha Franklin", "Lynn Collins", "Chaka Khan", "Etta James"],
-        answer: 2
+        answers: [
+            {text: "Aretha Franklin", correct: false},
+            {text: "Lynn Collins", correct: false},
+            {text: "Chaka Khan", correct: true},
+            {text: "Etta James", correct: false}
+        ]
     },
     {
         question: 'Which New Orleans base funk band release their hit "Cissy Strut" in 1969?',
-        answers: ["The Neville Brothers", "The Meters", "Allen Toussaint", "John Cleary"],
-        answer: 1
+        answers: [
+            {text: "The Neville Brothers", correct: false},
+            {text: "The Meters", correct: true},
+            {text: "Allen Toussaint", correct: false},
+            {text: "John Cleary", correct: false}
+        ]
     },
     {
         question: "Which of the following characteristics is not commonly found in funk music?",
-        answers: ["Syncopation", "Strong downbeats", "Seventh chord variants", "Grooves driven by violin"],
-        answer: 3
+        answers: [
+            {text: "Syncopation", correct: false},
+            {text: "Strong downbeats", correct: false},
+            {text: "Seventh chord variants", correct: false},
+            {text: "Grooves driven by violin", correct: true}
+        ]
     },
     {
         question: "Bootsy Collins, Larry Graham, George Porter, Jr., and Marcus Miller are all considered among the best funk players at which instrument?",
-        answers: ["Guitar", "Bass", "Drums", "Trombone"],
-        answer: 1
+        answers: [
+            {text: "Guitar", correct: false},
+            {text: "Bass", correct: true},
+            {text: "Drums", correct: false},
+            {text: "Trombone", correct: false}
+        ]
     },
     {
         question: "Jabo Starks, Clyde Stubblefield, Ziggaboo Modeliste, and David Garibaldi are all considered pioneers of funk on which instrument?",
-        answers: ["Guitar", "Bass", "Drums", "Trombone"],
-        answer: 2
+        answers: [
+            {text: "Guitar", correct: false},
+            {text: "Bass", correct: false},
+            {text: "Drums", correct: true},
+            {text: "Saxophone", correct: false}
+        ]
     },
     {
         question: "Although funk music is still around and popular in its own right, which other modern genre of music did it most inspire?",
-        answers: ["Country", "EDM", "Pop", "Hip hop"],
-        answer: 3
+        answers: [
+            {text:"Country", correct: false},
+            {text: "EDM", correct: false},
+            {text: "Pop", correct: false},
+            {text: "Hip hop", correct: true}
+        ]
     }
 ];
-
-// sets starting question to the first item of the array
-var currentQuestion = -1;
-
-var dynamicElements = [
-    screen0El,
-    screen1El,
-    screen2El,
-    highScoreEl,
-    timerEl,
-];
-
-function init() {
-    setEventListeners();
-}
-
-function setState(state) {
-    populateQuestion();
-
-    dynamicElements.forEach(function (ele) {
-        var possibleStatesAttr = ele.getAttribute("data-states");
-        var possibleStates = JSON.parse(possibleStatesAttr);
-        if (possibleStates.includes(state)) {
-            ele.classList.remove(hideClass);
-        } else {
-            ele.classList.add(hideClass);
-        }
-    });
-}
-
-// Timer that counts down from 100
-function runTimer() {
-    var timeLeft = 10;
-      var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        setState(0);
-      }
-    }, 1000);
-  }
-
-
-function populateQuestion() {
-    if (currentQuestion === questions.length - 1) {
-        return;
-    } else {
-        currentQuestion += 1;
-    }
-    var questionObj = questions[currentQuestion];
-    console.log(questionObj.question);
-    // Remove the current list items
-    answersEl.innerHTML = "<ul></ul>";
-    // Show question and possible answers
-    questionEl.textContent = questionObj.question;
-    questionObj.answers.forEach(function (answer) {
-        var li = document.createElement("li");
-        li.textContent = answer;
-            li.addEventListener("click", function () {
-       console.log(answer)
-            
-            // if (target.matches("li")) {
-            //   window.alert(target.innerText);
-            var rightAnswer = questions[currentQuestion].answer
-            var selectedAnswer = questionObj.answers.indexOf(answer)
-            if (selectedAnswer == rightAnswer) {
-                window.alert("correct");
-            } else
-            window.alert("wrong")
-        });
-        answersEl.appendChild(li);
-    });
-   
-    console.log(answersEl)
-    
-}
-
-function setEventListeners() {
-    screen0ButtonEl.addEventListener("click", function () {
-        setState(1);
-        runTimer();
-    });
-    screen1ButtonEl.addEventListener("click", function () {
-        setState(1);
-    });
-    screen2ButtonEl.addEventListener("click", function () {
-        setState(0);
-    });
-    /* answersEl.addEventListener("click", function (evt) {
-       
-        var target = evt.target.value;
-        console.log(target)
-        // if (target.matches("li")) {
-        //   window.alert(target.innerText);
-        console.log(questions[currentQuestion].answer)
-        if (target == questions[currentQuestion].answer) {
-            window.alert("correct");
-        } else
-        window.alert("wrong")
-    });*/
-}
-
-init();
